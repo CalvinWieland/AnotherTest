9185e5cf5930a546c6a67ff6855c6ff93dfa5a20
Calvin Wieland <66046869+CalvinWieland@users.noreply.github.com>
2023-09-16 19:56:13 -0700

    Delete cmake-example/build directory

--- cmake-example/build/CMakeFiles/CPTS_223_CMake_Example.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CPTS_223_CMake_Example.
-# This may be replaced when dependencies are built.
--- cmake-example/build/CMakeFiles/CPTS_223_CMake_Example.dir/factorial.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/CPTS_223_CMake_Example.dir/factorial.cpp.o: \
- /home/calvinwieland/CPTS-223-Examples/OceanExample/CPTS-223-Examples/cmake-example/factorial.cpp \
- /usr/include/stdc-predef.h \
- /home/calvinwieland/CPTS-223-Examples/OceanExample/CPTS-223-Examples/cmake-example/functions.h
